073c9b44d0b2f39f732778617941d99c90ddabfb
roylyh <roy.louyihui@gmail.com>
2023-02-06 16:21:38 -0800

    New assertEqual file

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    // console.assert(actual === expected, `✅✅✅ ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+  
+  
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
