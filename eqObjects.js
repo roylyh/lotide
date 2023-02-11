dc387aefa2cc0202216f059af9bdbeb7b5870b1a
roylyh <roy.louyihui@gmail.com>
2023-02-11 12:22:41 -0800

    refactor eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -1,44 +1,69 @@
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
-const eqObjects = function (object1, object2) {
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
+// const eqObjects = function (object1, object2) {
+//   const keys1 = Object.keys(object1);
+//   const keys2 = Object.keys(object2);
 
-  if (keys1.length !== keys2.length) {
-    return false;
-  }
+//   if (keys1.length !== keys2.length) {
+//     return false;
+//   }
 
-  for (let key of keys1) {
-    if (Array.isArray(object1[key])) {
-      if (!eqArrays(object1[key], object2[key])) {
-        return false;
-      }
-    } else if (object1[key] !== object2[key]) {
+//   for (let key of keys1) {
+//     if (Array.isArray(object1[key])) {
+//       if (!eqArrays(object1[key], object2[key])) {
+//         return false;
+//       }
+//     } else if (object1[key] !== object2[key]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+
+// refactor eqobject
+const eqObjects = function(obj1, obj2) {
+  // base case
+  if (obj1 === obj2) return true;
+  // check null and type
+  if (obj1 === null || obj2 === null || typeof obj1 !== "object" || typeof obj2 !== "object") return false;
+  // get the keys
+  let obj1Keys = Object.keys(obj1);
+  let obj2Keys = Object.keys(obj2);
+  // check the length of keys
+  if (obj1Keys.length !== obj2Keys.length) return false;
+  
+  for (let key of obj1Keys) {
+    //
+    if (!obj2Keys.includes(key)) {
+      return false;
+    }
+    // recursive case
+    if (!eqObjects(obj1[key],obj2[key])) {
       return false;
     }
   }
   return true;
 };
 
-const eqArrays = function (arr1, arr2) {
-  // compare the length of arr1
-  const length1 = arr1.length;
-  const length2 = arr2.length;
-  if (length1 !== length2) {
-    return false;
-    // compare the item inside one by one
-  } else {
-    for (let i = 0; i < length1; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
+// const eqArrays = function(arr1, arr2) {
+//   // compare the length of arr1
+//   const length1 = arr1.length;
+//   const length2 = arr2.length;
+//   if (length1 !== length2) {
+//     return false;
+//     // compare the item inside one by one
+//   } else {
+//     for (let i = 0; i < length1; i++) {
+//       if (arr1[i] !== arr2[i]) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+// };
 
 // FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -49,12 +74,11 @@ const assertEqual = function (actual, expected) {
 
 // TEST CODE
 // const shirtObject = { color: "red", size: "medium" };
-// const anotherShirtObject= { size: "medium", color: "red" };
+// const anotherShirtObject = { size: "medium", color: "red" };
 // eqObjects(shirtObject , anotherShirtObject); // => true
-//We need to use that return value in combination with assertEquals to test if the function is working correctly.
 // assertEqual(eqObjects(shirtObject , anotherShirtObject), true);
 
-// const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
+// const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
 // eqObjects(shirtObject , longSleeveShirtObject); // => false
 // assertEqual(eqObjects(shirtObject , longSleeveShirtObject), false);
 
@@ -78,3 +102,6 @@ const assertEqual = function (actual, expected) {
 //   eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject),
 //   false
 // );
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }),true);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }),false);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }),false);
