b5c01b2f14c5c146a8043b073a348972a6929bb4
roylyh <roy.louyihui@gmail.com>
2023-02-13 21:43:28 -0800

    Refactor with Mocha and Chai I

--- test/headTest.js
+++ test/headTest.js
@@ -1,10 +1,3 @@
-// const head = require("../head");
-// const assertEqual = require('../assertEqual');
-
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(head([5]), 5);
-// assertEqual(head([]), undefined);
 const assert = require("chai").assert;
 const head = require("../head");
 
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,23 +1,18 @@
 const tail = require("../tail");
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 
-// Test Case: Check the original array which has 3 elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, ["Lighthouse", "Labs"].length);
-assertEqual(result[0],"Lighthouse");
-assertEqual(result[1],"Labs");
+describe("#tail",() => {
+  
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
 
-// Test Case: Check the original array which has 0 elements
-const result1 = tail([]);
-assertEqual(result1.length, 0);
-assertEqual(result1[0],undefined);
+  it("returns [] for ['Hello']", () => {
+    assert.deepEqual(tail(['Hello']), []);
+  });
 
-// Test Case: Check the original array which has 1 element
-const result2 = tail(["Hello"]);
-assertEqual(result2.length, 0);
-assertEqual(result2[0],undefined);
+});
